eba304ddcb503faec42f6f1bee5b81b0fb823b07
Yakir Adela <yakirade@gmail.com>
2025-07-28 11:13:24 +0300

    Update train.py

--- automl-platform/trainer/train.py
+++ automl-platform/trainer/train.py
@@ -1,8 +1,8 @@
 import boto3
-import pandas as a
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
-import os 
+import os
 
 s3 = boto3.client("s3")
 s3_key = os.environ["S3_KEY"]
@@ -10,13 +10,13 @@ s3_key = os.environ["S3_KEY"]
 s3.download_file("automl-csv-data", s3_key, "data.csv")
 df = pd.read_csv("data.csv")
 
-x = df.drop("target, axis=1")
+x = df.drop("target", axis=1)
 y = df["target"]
 
 model = LogisticRegression()
-model.fit(x,y)
+model.fit(x, y)
 
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-s3.upload_file("model.pkl", "automl-csv-data", f"models/{s3_key}.pkl")    
+s3.upload_file("model.pkl", "automl-csv-data", f"models/{s3_key}.pkl")
