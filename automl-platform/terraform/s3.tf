055f6fc40364806fefc8711653b400433ce68f89
Yakir Adela <yakirade@gmail.com>
2025-07-28 19:44:31 +0300

    Update s3.tf

--- automl-platform/terraform/s3.tf
+++ automl-platform/terraform/s3.tf
@@ -1,32 +1,9 @@
-resource "aws_s3_bucket" "csv_data" {
-  bucket        = var.s3_bucket_name
-  force_destroy = true
-
-  versioning {
-    enabled = true
+terraform {
+  backend "s3" {
+    bucket          = "terraform-state-bucketxyz123"
+    key             = "eks/terraform.tfstate"
+    region          = "us-east-2"
+    use_lockfile    = true
+    encrypt         = true
   }
-  
-  tags = {
-    Name        = var.s3_bucket_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.csv_data.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.csv_data.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
