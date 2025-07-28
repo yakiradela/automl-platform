2ee2e9c9a38563b7f0f70d5ed68dc2dbdeb925f4
Yakir Adela <yakirade@gmail.com>
2025-07-28 11:14:41 +0300

    Update s3.py

--- automl-platform/api/services/s3.py
+++ automl-platform/api/services/s3.py
@@ -3,5 +3,5 @@ import boto3
 def upload_to_s3(file):
     s3 = boto3.client("s3")
     key = f"uploads/{file.filename}"
-    s3.upload_fileobj(file, "automtl-csv-data", key)
-    return
+    s3.upload_fileobj(file, "automl-csv-data", key)  # ודא שהבאקט קיים ושמו נכון
+    return key
