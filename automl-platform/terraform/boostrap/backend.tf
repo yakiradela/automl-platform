6a7ddd4b43a50bd32937abc87d0e0e2f69f278e0
Yakir Adela <yakirade@gmail.com>
2025-07-28 19:31:40 +0300

    Update backend.tf

--- automl-platform/terraform/boostrap/backend.tf
+++ automl-platform/terraform/boostrap/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket"  # שים כאן את שם הבאקט לאחסון ה-state
+    bucket = "terraform-state-bucketxyz123"  # שים כאן את שם הבאקט לאחסון ה-state
     key    = "state/terraform.tfstate"      # נתיב בתוך הבאקט (אפשר פשוט terraform.tfstate)
     region = "us-east-2"                     # אזור הבאקט
   }
