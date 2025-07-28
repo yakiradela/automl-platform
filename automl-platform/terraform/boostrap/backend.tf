2ed756105aa4fe43f8a8b741866c658efdd57dc3
Yakir Adela <yakirade@gmail.com>
2025-07-28 11:02:33 +0300

    Update backend.tf

--- automl-platform/terraform/boostrap/backend.tf
+++ automl-platform/terraform/boostrap/backend.tf
@@ -1,36 +1,12 @@
 resource "aws_s3_bucket" "csv_data" {
-  bucket        = var.s3_bucket_name
+  bucket        = "your-new-csv-bucket-name"
   force_destroy = true
 
+  versioning {
+    enabled = true
+  }
+  
   tags = {
-    Name        = var.s3_bucket_name
-    Environment = var.environment
+    Name = "CSV Data Bucket"
   }
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.csv_data.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.csv_data.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.csv_data.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
